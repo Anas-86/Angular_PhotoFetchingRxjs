0eb0fe9e4d24597244df5ede0dfbcf118741531d
Anas <anas.kala@hotmail.com>
2023-10-12 21:26:45 +0200

    adding an API using RXJS (observable and observer)

--- src/app/photo-show/photo-show.component.ts
+++ src/app/photo-show/photo-show.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { PhotosService } from '../photos.service';
 
 @Component({
   selector: 'app-photo-show',
   templateUrl: './photo-show.component.html',
   styleUrls: ['./photo-show.component.css']
 })
-export class PhotoShowComponent {
 
+
+export class PhotoShowComponent {
+  photoUrl: string = '';
+
+  constructor(private photoService: PhotosService) {
+    this.fetchPhoto();
+  }
+
+  onClick() {
+    this.fetchPhoto();
+  }
+
+  fetchPhoto() {
+    this.photoService.getPhoto().subscribe((response) => {
+      this.photoUrl = response.urls.regular;
+    });
+  }
 }
--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPhoto() {
+    return this.http.get<UnsplashResponse>('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: 'Client-ID VBoEDrf1khZcr16FCdAF90Mf9M5GW6KGi7FOmsJD5gw'
+      }
+    });
+  }
 }
